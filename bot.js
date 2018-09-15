49bc44e062d0a786070c048d399ef4598d8476fb
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-09-15 12:41:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ if (message.content.startsWith(adminprefix + 'setgame')) {
   client.user.setGame(argresult);
     message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
 } else 
-  if (message.content.startsWith(adminprefix + 'setname')) {
+  if (message.content.startsWith(prefix + 'setname')) {
 client.user.setUsername(argresult).then
     message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
 return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
