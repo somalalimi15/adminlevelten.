70aa873650f0943a8dd20f212e9d8498411297b4
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-09-15 18:28:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -46,6 +46,23 @@ if (message.content.startsWith(adminprefix + 'setT')) {
   }
 });
   
+client.on('message', message => {
+    if(message.content.includes('discord.gg/')) {
+        if(message.member.hasPermission('ADMINISTRATOR')) return;
+        message.delete();
+        message.guild.member(message.author).addRole(message.guild.roles.find(r => r.name === 'Muted'));
+        let embedP = new Discord.RichEmbed()
+        .setTitle('❌ | تمت معاقبتك')
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر روابط اضافة الى سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الاسكات عن طريق الخطأ الرجاء التوجه والتكلم مع الادآرة**`)
+        .addField(`by`,`Hello,Im'iiLizx`)
+        .setColor('RED')
+        .setThumbnail(message.author.avatarURL)
+        .setFooter(`${message.guild.name} Server`, message.guild.iconURL)
+        
+        message.channel.send(embedP);
+    }
+});
 
 client.login(process.env.BOT_TOKEN);
 
