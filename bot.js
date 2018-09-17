73b62770b76c822fe48d9593c5ffd7b73662747f
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-09-17 18:39:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -703,5 +703,28 @@ Servers Counter : __${client.guilds.size}__**`)
 
 );
 
+client.on('message',async msg => {
+  var p = "!";
+  if(msg.content.startsWith(p + "user")) {
+  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
+  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
+  msg.guild.createChannel(`يتم تحضير الروم :[]` , 'voice').then(time => {
+    time.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+  setInterval(() => {
+      var currentTime = new Date(),
+Year = currentTime.getFullYear(),
+Month = currentTime.getMonth() + 1,
+Dat = currentTime.getDate()
+      time.setName(`Members : ◤ → ${client.users.size} ← ◢`);
+ },1000);
+  });
+  }
+ 
+});
+
+
 client.login(process.env.BOT_TOKEN);
 
