b1de6d503dcc956b5969090189738af8f2efa7e0
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-09-18 10:21:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -907,8 +907,6 @@ client.on("message", async message => {
         }
         })
 
-const client = new Discord.Client();
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
